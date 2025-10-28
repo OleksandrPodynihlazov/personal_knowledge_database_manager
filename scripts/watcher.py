74f3f73957276ba9d36aacf93c79c89edcb8ee61
Oleksandr Podynihlazov <x00215403@mytudublin.ie>
2025-10-28 21:37:27 +0000

    Add flake8 configuration, enhance watcher logging, and improve file processing

--- scripts/watcher.py
+++ scripts/watcher.py
@@ -1,19 +1,57 @@
 import time
-from watchdog.events import FileSystemEventHandler  # type: ignore
-from watchdog.observers import Observer  # type: ignore
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 
 class Watcher(FileSystemEventHandler):
-    def on_any_event(self, event):
-        print(f"Event type {event.event_type} occured on {event.src_path}")
+    def on_created(self, event):
+        """
+        Called when a file or directory is created within the directory that is being watched.
+        Logs a message to the console indicating that a new file has been detected.
+        :param event: A FileSystemEvent object that contains information about the file that was created.
+        """
+        logging.info(f"New file detected: {event.src_path}")
+        process_file(event.src_path)
 
 
-event_hander = Watcher()
-observer = Observer()
-observer.schedule(event_hander, path='.', recursive=False)
-observer.start()
-print("Started watching current directory for changes...")
-time.sleep(10)
+def start_watching(path: str):
+    """
+    Start watching the given directory for any changes.
 
-observer.stop()
-observer.join()
+    :param path: The path to the directory that should be watched for changes.
+    """
+    event_hander = Watcher()
+    observer = Observer()
+    observer.schedule(event_hander, path=path, recursive=False)
+    observer.start()
+
+    logging.info(f"Started watching directory {path}")
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        logging.info(f"Stopped watching directory {path}")
+        observer.join()
+
+
+def process_file(file_path: bytes | str):
+    """
+    Process the newly created file.
+
+    :param file_path: The path to the newly created file.
+    """
+    logging.info(f"Processing file: {file_path}")
+
+
+if __name__ == "__main__":
+    watch_path = "../inbox"
+    start_watching(watch_path)
