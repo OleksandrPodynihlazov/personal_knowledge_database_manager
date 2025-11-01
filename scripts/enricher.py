f98ca3871edd553b5a1d74bd8b91ace3b3eda98f
Oleksandr Podynihlazov <x00215403@mytudublin.ie>
2025-11-01 16:26:54 +0000

    Add KBIntegrator class for note creation and enhance NerEnricher with enriched data handling

--- scripts/enricher.py
+++ scripts/enricher.py
@@ -2,8 +2,16 @@ import spacy
 from scripts.classifier import ClassifiedData
 from scripts.logging_config import setup_logging
 import logging
+from dataclasses import dataclass, field
+from typing import List, Dict
+from collections import defaultdict
 
 setup_logging()
+
+
+@dataclass
+class EnrichedData(ClassifiedData):
+    entities: Dict[str, List[str]] = field(default_factory=dict)
 
 
 class NerEnricher:
@@ -18,20 +26,25 @@ class NerEnricher:
         """
         self.nlp = spacy.load("en_core_web_sm")
 
-    def enrich(self, data: ClassifiedData) -> ClassifiedData | None:
+    def enrich(self, data: ClassifiedData) -> EnrichedData | None:
         """
         Enriches the given data with named entities using the spaCy library.
 
         :param data: The data that should be enriched with named entities.
         :return: The enriched data, or None if an error occurred during the enrichment process.
         """
+        enriched_data = EnrichedData(text=data.text,
+                                     source_path=data.source_path,
+                                     category=data.category,
+                                     tags=data.tags)
         try:
             s_obj = self.nlp(data.text)
-            entities = []
+            entities = defaultdict(list)
             for ent in s_obj.ents:
-                entities.append(f"{ent.text} ({ent.label_})")
-            data.entities = entities
-            return data
+                entities[ent.label_].append(ent.text)
+            entities = dict(entities)
+            enriched_data.entities = entities
+            return enriched_data
         except Exception as e:
             logging.error(f"Error during NER enrichment for file {data.source_path}: {e}")
             return None
